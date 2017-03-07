257c9dc5322b4a62da84d052dc75ce1a257267b7
Jesse Myers <jesse.myers@globality.com>
2017-03-06 19:52:10 -0800

    Add build info convention

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
             "app = microcosm_flask.factories:configure_flask_app",
             "audit = microcosm_flask.audit:configure_audit_decorator",
             "basic_auth = microcosm_flask.basic_auth:configure_basic_auth_decorator",
+            "build_info_convention = microcosm_flask.conventions.build_info:configure_build_info",
             "discovery_convention = microcosm_flask.conventions.discovery:configure_discovery",
             "error_handlers = microcosm_flask.errors:configure_error_handlers",
             "flask = microcosm_flask.factories:configure_flask",
